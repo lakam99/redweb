bf157f49cab1ec10a09af49b6e84a6e2cd5f6430
Arkam Mazrui <superarkam@gmail.com>
2025-01-08 18:18:38 -0500

    add server-side htmx rendering

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { METHODS } = require('./src/http');
 const { sendJson } = require('./src/ws/util');
 const { SocketServer, SecureSocketServer, SOCKET_OPTIONS, SocketRoute } = require('./src/ws');
 const { BaseHandler } = require('./src/ws/BaseHandler');
+const HttpServer = require('./src/http/HttpServer');
+const HttpsServer = require('./src/http/HttpsServer');
 module.exports = {
+    HttpServer,
+    HttpsServer,
     SocketServer,
     SecureSocketServer,
     BaseHandler,
